757dd7c26587862dbd61d50e6a0657b144f08b6e
Gurglemurgle <95941332+gurglemurgle5@users.noreply.github.com>
2026-01-19 22:18:06 -0600

    Fix filler items leaking into generation (#3, #4)

--- worlds/rhythm_doctor/Data.py
+++ worlds/rhythm_doctor/Data.py
@@ -252,6 +252,8 @@ all_stages = all_regular_stages + all_boss_stages
 # endregion
 
 all_items = all_regular_stages + FILLER + KEYS
+
+all_progression_items = all_regular_stages + KEYS
 # endregion
 
 
@@ -284,7 +286,7 @@ def create_items(world: "RhythmDoctorWorld"):
     total_locations = len(world.multiworld.get_unfilled_locations(world.player))
     item_pool = []
 
-    for item in all_items:
+    for item in all_progression_items:
         if (
             world.options.end_goal.value == EndGoal.option_helping_hands
             and isinstance(item, _RegularStage)
